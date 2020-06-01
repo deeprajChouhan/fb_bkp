e967177091e7537b1e9bba144792e7943d4c2a29
deeprajChouhan <codeblooded018@gmail.com>
2020-06-02 03:44:10 +0530

    Initial commit

---
+++ app/Views/footer.php
@@ -0,0 +1,171 @@
+<section class="footer-area section-bg-2 padding-top-100px padding-bottom-40px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 column-td-half">
+                <div class="footer-widget">
+                    <a href="index.html">
+                        <img src="images/logo2.png" alt="footer logo" class="footer__logo">
+                    </a>
+                    <ul class="list-items footer-address">
+                        <li><a href="tel:+1631237884">+163 123 7884</a></li>
+                        <li><a href="mailto:support@wbsite.com" class="mail">support@website.com</a></li>
+                        <li>Melbourne, Australia, 105 South Park Avenue</li>
+                    </ul>
+                    <h3 class="widget-title font-size-17 mt-4">We are on</h3>
+                    <ul class="social-profile">
+                        <li><a href="about.html#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div><!-- end footer-widget -->
+            </div><!-- end col-lg-3 -->
+            <div class="col-lg-3 column-td-half">
+                <div class="footer-widget">
+                    <h3 class="widget-title">Company</h3>
+                    <span class="section-divider"></span>
+                    <ul class="list-items">
+                        <li><a href="about.html#">about us</a></li>
+                        <li><a href="about.html#">contact us</a></li>
+                        <li><a href="about.html#">become a Teacher</a></li>
+                        <li><a href="about.html#">support</a></li>
+                        <li><a href="about.html#">FAQs</a></li>
+                        <li><a href="about.html#">blog</a></li>
+                    </ul>
+                </div><!-- end footer-widget -->
+            </div><!-- end col-lg-3 -->
+            <div class="col-lg-3 column-td-half">
+                <div class="footer-widget">
+                    <h3 class="widget-title">Courses</h3>
+                    <span class="section-divider"></span>
+                    <ul class="list-items">
+                        <li><a href="about.html#">Web Development</a></li>
+                        <li><a href="about.html#">Hacking</a></li>
+                        <li><a href="about.html#">PHP Learning</a></li>
+                        <li><a href="about.html#">Spoken English</a></li>
+                        <li><a href="about.html#">Self-Driving Car</a></li>
+                        <li><a href="about.html#">Garbage Collectors</a></li>
+                    </ul>
+                </div><!-- end footer-widget -->
+            </div><!-- end col-lg-3 -->
+            <div class="col-lg-3 column-td-half">
+                <div class="footer-widget">
+                    <h3 class="widget-title">Download App</h3>
+                    <span class="section-divider"></span>
+                    <ul class="btn-box">
+                        <li>
+                            <a href="about.html#" class="theme-btn">
+                                <i class="la la-apple icon-element"></i>
+                                <span class="app-titles">
+                                    <span class="app__subtitle">Download on the</span>
+                                    <span class="app__title">App Store</span>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="about.html#" class="theme-btn">
+                                <i class="la la-android icon-element"></i>
+                                <span class="app-titles">
+                                    <span class="app__subtitle">Get in on</span>
+                                    <span class="app__title">Google Play</span>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div><!-- end footer-widget -->
+            </div><!-- end col-lg-3 -->
+        </div><!-- end row -->
+        <div class="copyright-content">
+            <div class="row align-items-center">
+                <div class="col-lg-10">
+                    <p class="copy__desc">&copy; 2020 Fireblaze. All Rights Reserved. by <a href="https://fireblaze.in/">Fireblaze Technologies Private Limited.</a></p>
+                </div><!-- end col-lg-9 -->
+                <div class="col-lg-2">
+                    <div class="sort-ordering">
+                        <select class="sort-ordering-select">
+                            <option value="1">English</option>
+                            <option value="2">Deutsch</option>
+                            <option value="3">Español</option>
+                            <option value="4">Français</option>
+                            <option value="5">Bahasa Indonesia</option>
+                            <option value="6">Bangla</option>
+                            <option value="7">日本語</option>
+                            <option value="8">한국어</option>
+                            <option value="9">Nederlands</option>
+                            <option value="10">Polski</option>
+                            <option value="11">Português</option>
+                            <option value="12">Română</option>
+                            <option value="13">Русский</option>
+                            <option value="14">ภาษาไทย</option>
+                            <option value="15">Türkçe</option>
+                            <option value="16">中文(简体)</option>
+                            <option value="17">中文(繁體)</option>
+                            <option value="17">Hindi</option>
+                        </select>
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end copyright-content -->
+    </div><!-- end container -->
+</section><!-- end footer-area -->
+<!-- ================================
+          END FOOTER AREA
+================================= -->
+
+<!-- start scroll top -->
+<div id="scroll-top">
+    <i class="fa fa-angle-up" title="Go top"></i>
+</div>
+<!-- end scroll top -->
+<div class="modal-form">
+    <div class="modal fade team-modal-form" id="teamModal" tabindex="-1" role="dialog" aria-labelledby="teamModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-top mb-0">
+                    <h3 class="modal-title widget-title font-size-20">New message</h3>
+                    <button type="button" class="close close-arrow" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="la la-close mb-0"></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="modal-text">
+                        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+                    </div>
+                    <div class="section-block"></div>
+                    <h3 class="widget-title font-size-16 pt-3">Connect me on</h3>
+                    <ul class="social-profile pt-3">
+                        <li><a href="about.html#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="about.html#"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- template js files -->
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-select.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/magnific-popup.min.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/waypoint.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/fancybox.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/date-time-picker.js"></script>
+<script src="js/jquery.filer.min.js"></script>
+<script src="js/emojionearea.min.js"></script>
+<script src="js/smooth-scrolling.js"></script>
+<script src="js/tooltipster.bundle.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ app/Views/header.php
@@ -0,0 +1,28 @@
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="author" content="TechyDevs">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Fireblaze AI School</title>
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" sizes="16x16" href="images/favicon.png">
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/line-awesome.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/fancybox.css">
+    <link rel="stylesheet" href="css/tooltipster.bundle.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- end inject -->
+</head>
